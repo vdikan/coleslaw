7cbb7acec188d82c6c85ebf266902a7cb8df9f26
Vladimir Dikan <vdikan@vivaldi.net>
2020-08-31 01:37:23 +0200

    other meta for opengraph

--- plugins/opengraph.lisp
+++ plugins/opengraph.lisp
@@ -4,12 +4,15 @@
   (:import-from :coleslaw #:add-injection
                           #:ogimage-of
                           #:title-of
+                          #:page-url
                           #:post))
 
 (in-package :coleslaw-opengraph)
 
 (defvar *og-metas*
   "~%
+<meta property=\"og:title\" content=\"~a\" />
+<meta property=\"og:url\" content=\"~a/~a\" />
 <meta property=\"og:image\" content=\"~a/~a\" />
 <meta property=\"og:image:secure_url\" content=\"~a/~a\" />
 <meta property=\"og:image:alt\" content=\"~a\" />")
@@ -18,9 +21,9 @@
   (flet ((inject-p (x)
            (when (typep x 'post)
              (format nil *og-metas*
-                     og-base-url
-                     (or (ogimage-of x) og-default-image)
-                     og-base-url
-                     (or (ogimage-of x) og-default-image)
-                     (title-of x)))))
+                     (title-of x)
+                     og-base-url (page-url x)
+                     og-base-url (or (ogimage-of x) og-default-image)
+                     og-base-url (or (ogimage-of x) og-default-image)
+                     (format nil "Image for: ~a" (title-of x))))))
     (add-injection #'inject-p :head)))
