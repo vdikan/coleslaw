54d5e924c82b3d049cdc00bc138b8077d3bebc66
Vladimir Dikan <vdikan@vivaldi.net>
2021-04-11 21:44:20 +0300

    preliminary parsing with custom cl-org-mode and trivia

--- plugins/org-books.lisp
+++ plugins/org-books.lisp
@@ -1,30 +1,96 @@
-;; (defpackage :coleslaw-org-books
-;;   (:use :cl)
-;;   (:export #:enable)
-;;   (:import-from :coleslaw
-;;                 #:*config*
-;;                 #:assert-field
-;;                 #:index
-;;                 #:content
-;;                 #:find-all
-;;                 #:by-date
-;;                 #:render
-;;                 #:publish
-;;                 #:theme-fn
-;;                 #:render-text
-;;                 #:add-document
-;;                 #:write-document))
+(ql:quickload :coleslaw)
+(ql:quickload :cl-org-mode)
+(ql:quickload :trivia)
 
-;; (in-package :coleslaw-org-books)
+(defpackage :coleslaw-org-books
+  (:use :cl)
+  (:export #:enable)
+  (:import-from :coleslaw
+                #:*config*
+                #:assert-field
+                #:index
+                #:content
+                #:find-all
+                #:by-date
+                #:render
+                #:publish
+                #:theme-fn
+                #:render-text
+                #:add-document
+                #:write-document))
+
+(in-package :coleslaw-org-books)
 
 ;;FIXME: For plugin-like package definition to work I need
 ;; to create instances with `coleslaw::url`-like fields.
 ;; Same is done in `static-pages` (initialize-instance).
 ;; That should fix `sitemap` too.
-(in-package :coleslaw)
+;; (in-package :coleslaw)
 
 
 ;; (defclass book (content) ())
+
+
+(defvar *org-src* #p"~/Grimoire/org/brain/Books.org")
+
+
+(defvar shelf-categories
+  '(("Computer Science" compsci)
+    ("Social Science"   social)
+    ("Natural Science"  natural)
+    ("Fiction"          fiction)))
+
+
+(defun shelf-for-category (name)
+  (second (assoc name shelf-categories :test #'string-equal)))
+
+
+(defun parse-org-src ()
+  (cl-org-mode-raw:org-raw-parse *org-src*))
+
+
+(defun get-revised ()
+  (nth 3 (third
+          (rest (parse-org-src)))))
+
+
+(defun get-categories ()
+  (trivia:match (get-revised)
+    ((cons :entry
+           (cons '(:STARS 2 :TITLE "Revised")
+                 (cons x y))) y)))
+
+
+(defun get-category-name (category)
+  (trivia:match category
+    ((list* :entry
+            (list :stars _ :title name) _)
+     name)))
+
+
+(defun parse-entry (entry shelf-symbol)
+  (trivia:match entry
+    ((list :ENTRY (list :STARS _ :TITLE title)
+           (list :SECTION
+                 (list :PROPERTY-DRAWER "PROPERTIES" :CONTENTS
+                       (list (list :PROPERTY "AUTHOR" :VALUE author)
+                             (list :PROPERTY "ADDED" :VALUE added-date)
+                             (list :PROPERTY "GOODREADS" :VALUE goodreads)
+                             _))
+                 review-text))
+     (list :title title
+           :author author
+           :added-date added-date
+           :goodreads goodreads
+           :review-text review-text
+           :shelf-symbol shelf-symbol))))
+
+
+(loop for category in (get-categories)
+      do (let* ((category-name (get-category-name category))
+                (shelf-symbol (shelf-for-category category-name)))
+           (loop for entry in (cdddr category)
+                 do (print (parse-entry entry shelf-symbol)))))
 
 
 ;;;SHELF
